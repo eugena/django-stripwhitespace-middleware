46ba1cb32e267c68ab7d8a50955d37519e33c5bf
eugena <eugena@powertofly.com>
2015-07-03 18:32:32 +0500

    Create middleware.py

---
+++ middleware.py
@@ -0,0 +1,25 @@
+import re
+
+class StripWhitespaceMiddleware(object):
+    """
+    Strips leading and trailing whitespace from response content.
+    """
+
+    def __init__(self):
+        self.whitespace = re.compile('^\s*\n', re.MULTILINE)
+        self.whitespace_lead = re.compile('^\s+', re.MULTILINE)
+        self.whitespace_trail = re.compile('\s+$', re.MULTILINE)
+
+
+    def process_response(self, request, response):
+        if "text" in response['Content-Type']:
+            if hasattr(self, 'whitespace_lead'):
+                response.content = self.whitespace_lead.sub('', response.content)
+            if hasattr(self, 'whitespace_trail'):
+                response.content = self.whitespace_trail.sub('\n', response.content)
+            # Uncomment the next line to remove empty lines
+            if hasattr(self, 'whitespace'):
+                response.content = self.whitespace.sub('', response.content)
+            return response
+        else:
+            return response
